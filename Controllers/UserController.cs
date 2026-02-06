77d9eeaf216bfdf2e44698caaeba9605551a36ed
maribortoluccicar <maribortoluccicar@gmail.com>
2026-02-06 20:21:36 -0300

    Get User Controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,7 +6,12 @@ namespace Closetly.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        
+        //criar usuario
+        //IActionResult de 200 à 500
+        public IActionResult CreateUser([FromBody] TbUser user)
+         
+            
+            //updateUser ---- Géssica
 
   
         
