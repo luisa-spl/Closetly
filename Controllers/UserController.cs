49f9cfe2131f2f4efaf9fb7b9873ad688ad1f45d
Gessica Azevedo <gessicamendonca.a@gmail.com>
2026-02-06 20:12:17 -0300

    feat: adds UpdatEUser endpoint

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,3 +1,5 @@
+using Closetly.DTO;
+using Closetly.Services.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Closetly.Controllers
@@ -6,8 +8,23 @@ namespace Closetly.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        
+        private readonly IUserService _userService;
 
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPatch("{id}", Name = "UpdateUser")]
+        public IActionResult UpdateUser([FromRoute] Guid id, [FromBody] UpdateUserRequest request)
+        {
+            string error = _userService.UpdateUser(id, request.Name, request.Phone, request.Email);
+            if(error == "error")
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
   
         
     }
