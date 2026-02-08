71db8b96c5bf41c8ff7386883c7d7084b8c1304c
Gessica Azevedo <gessicamendonca.a@gmail.com>
2026-02-08 14:22:12 -0300

    refactor: remove unused DTO import from IUserService interface

--- Services/Interface/IUserService.cs
+++ Services/Interface/IUserService.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Closetly.DTO;
 using Closetly.Models;
 
 namespace Closetly.Services.Interface
