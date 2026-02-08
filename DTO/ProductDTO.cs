e59ee467b17b9c5091c7bfa1e163391e334d19ab
Gessica Azevedo <gessicamendonca.a@gmail.com>
2026-02-08 14:21:45 -0300

    feat: add ProductDTO to represent product details in the application

---
+++ DTO/ProductDTO.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Closetly.DTO
+{
+    public class ProductDTO
+    {
+        public Guid ProductId { get; set; }
+
+        public string ProductColor { get; set; } = null!;
+
+        public string ProductSize { get; set; } = null!;
+
+        public string ProductType { get; set; } = null!;
+
+        public string ProductOccasion { get; set; } = null!;
+
+        public string ProductStatus { get; set; } = null!;
+
+        public decimal ProductValue { get; set; }
+    }
+}
