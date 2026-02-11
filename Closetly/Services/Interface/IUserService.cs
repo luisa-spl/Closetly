2c3ae100a0680dd74bda9379611406cb339f03da
Gessica Azevedo <gessicamendonca.a@gmail.com>
2026-02-11 19:46:47 -0300

    feat: add GetUsers method to IUserRepository and IUserService for user retrieval

--- Closetly/Services/Interface/IUserService.cs
+++ Closetly/Services/Interface/IUserService.cs
@@ -12,5 +12,6 @@ namespace Closetly.Services.Interface
         public string UpdateUser(Guid id, string newUserName, string newPhone, string newEmail);
         public UserDTO CreateUser(UserDTO user); //criacao de usuario       
         public List<UserOrders>? GetUserOrders(Guid userId);
+        public List<UserDTO> GetUsers();
     }
 }
