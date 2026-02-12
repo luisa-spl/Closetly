734d26a148995cb3f6973466fd9bac3679bd5936
Gessica Azevedo <gessicamendonca.a@gmail.com>
2026-02-11 21:19:01 -0300

    feat: add static classes for OrderStatus, PaymentStatus, ProductOccasion, ProductSize, ProductStatus, and ProductType with predefined values

---
+++ Closetly/Models/ProductOccasion.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Closetly.Models
+{
+    public static class ProductOccasion
+    {
+        public static readonly string CASUAL = "CASUAL";
+        public static readonly string FORMAL = "FORMAL";
+        public static readonly string PARTY = "PARTY";
+        public static readonly string WEDDING = "WEDDING";
+    }
+}
---
+++ Closetly/Models/ProductType.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Closetly.Models
+{
+    public static class ProductType
+    {
+        public static readonly string DRESS = "DRESS";
+        public static readonly string SUIT = "SUIT";
+        public static readonly string SHIRT = "SHIRT";
+        public static readonly string TSHIRT = "TSHIRT";
+        public static readonly string PANTS = "PANTS";
+        public static readonly string SKIRT = "SKIRT";
+        public static readonly string JACKET = "JACKET";
+        public static readonly string COAT = "COAT";
+        public static readonly string SHOES = "SHOES";
+        public static readonly string ACCESSORY = "ACCESSORY";
+    }
+}
