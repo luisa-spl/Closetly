16f40d6ecb696a47b1e97d2f281dc5bbc24437cc
luisa spinola <luisa.spinola@cubos.academy>
2026-02-17 20:29:02 -0300

    feat: adding mappers and validators

---
+++ Closetly/Application/Mappers/NewOrderMapper.cs
@@ -0,0 +1,28 @@
+﻿using Closetly.DTO;
+using Closetly.Models;
+
+namespace Closetly.Application.Mappers;
+
+public static class NewOrderMapper
+{
+    public static OrderResponseDTO MapToOrderResponseDTO(TbOrder order)
+    {
+        var response = new OrderResponseDTO
+        {
+            Id = order.OrderId,
+            OrderedAt = order.OrderedAt,
+            ReturnDate = order.ReturnDate,
+            Total = order.OrderTotalValue,
+            UserId = order.UserId,
+            PaymentStatus = "PENDING",
+            Products = order.TbOrderProducts.Select(p => new OrderProductResponseDTO
+            {
+                ProductId = p.ProductId,
+                Quantity = p.Quantity,
+               
+            }).ToList()
+        };
+
+        return response;
+    }
+}
