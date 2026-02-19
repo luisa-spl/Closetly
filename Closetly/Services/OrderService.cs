829e4f067efa3b764b7938bfa1a562f94851271b
Gessica Azevedo <gessicamendonca.a@gmail.com>
2026-02-18 22:45:26 -0300

    fix: changes strings to use static constants for order and product properties

--- Closetly/Services/OrderService.cs
+++ Closetly/Services/OrderService.cs
@@ -44,7 +44,7 @@ public class OrderService : IOrderService
             OrderId = Guid.NewGuid(),
             OrderedAt = dateNow,
             ReturnDate = dateNow.AddDays(order.ReturnPeriod),
-            OrderStatus = "PENDING"
+            OrderStatus = OrderStatus.PENDING
         };
 
         decimal total = 0;
@@ -67,7 +67,7 @@ public class OrderService : IOrderService
             
         }
 
-        await OrderValidator.ChangeManyProductsStatus(_productRepository, orderProducts, "UNAVAILABLE");
+        await OrderValidator.ChangeManyProductsStatus(_productRepository, orderProducts, ProductStatus.UNAVAILABLE);
         newOrder.TbOrderProducts = orderProducts;
         newOrder.OrderTotalItems = orderProducts.Count();
         newOrder.OrderTotalValue = total;
@@ -85,15 +85,15 @@ public class OrderService : IOrderService
             throw new InvalidOperationException($"Pedido com Id '{orderId}' não encontrado");
         }
 
-        if (order.OrderStatus != "PENDING")
+        if (order.OrderStatus != OrderStatus.PENDING)
         {
             throw new InvalidOperationException($"Pedido com Id '{orderId}' não pode ser cancelado pois já foi pago e/ou está concluido");
         }
 
-        order.OrderStatus = "CANCELLED";
+        order.OrderStatus = OrderStatus.CANCELLED;
         var orderProducts = order.TbOrderProducts.ToList();
 
-        await OrderValidator.ChangeManyProductsStatus(_productRepository, orderProducts, "AVAILABLE");
+        await OrderValidator.ChangeManyProductsStatus(_productRepository, orderProducts, ProductStatus.AVAILABLE);
 
         await _repository.CancelOrder(order);
     }
